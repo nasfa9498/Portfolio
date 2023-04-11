f919c9fa603b2781768e90ba7da982107803478c
nasfa9498 <nasfafarveen9498@gmail.com>
2023-04-11 12:53:50 +0530

    Initial commit:Set up project structure and configuration files

---
+++ portfolio.js
@@ -0,0 +1,91 @@
+document.getElementById("button1").addEventListener("click",function(){
+    var profile = document.getElementById("myprofile");
+if (profile.style.display === "none") {
+  profile.style.display = "block";
+} else {
+  profile.style.display = "none";
+}
+});
+
+document.getElementById("button2").addEventListener("click",function(){
+    var Edu = document.getElementById("Education");
+if ( Edu.style.display === "none") {
+    Edu.style.display = "flex";
+}
+
+else {
+    Edu.style.display = "none";
+}
+});
+
+document.getElementById("button3").addEventListener("click",function(){
+    var exp = document.getElementById("Experience");
+if (exp.style.display === "none") {
+    exp.style.display = "flex";
+} else {
+    exp.style.display = "none";
+}
+});
+
+document.getElementById("button4").addEventListener("click",function(){
+    var skill = document.getElementById("Skills");
+if (skill.style.display === "none") {
+    skill.style.display = "flex";
+} else {
+    skill.style.display = "none";
+}
+});
+
+document.getElementById("button5").addEventListener("click",function(){
+    var cont = document.getElementById("Contact");
+if (cont.style.display === "none") {
+    cont.style.display = "flex";
+} else {
+    cont.style.display = "none";
+}
+});
+// Skills part
+document.getElementById("button6").addEventListener("click",function(){
+    var front = document.getElementById("front");
+if (front.style.display === "none") {
+    front.style.display = "flex";
+} else {
+    front.style.display = "none";
+}
+});
+
+document.getElementById("button7").addEventListener("click",function(){
+    var quality = document.getElementById("quality");
+if (quality.style.display === "none") {
+    quality.style.display = "flex";
+} else {
+    quality.style.display = "none";
+}
+});
+
+document.getElementById("button8").addEventListener("click",function(){
+    var analys = document.getElementById("analys");
+if (analys.style.display === "none") {
+    analys.style.display = "flex";
+} else {
+    analys.style.display = "none";
+}
+});
+
+document.getElementById("button9").addEventListener("click",function(){
+    var lang = document.getElementById("lang");
+if (lang.style.display === "none") {
+    lang.style.display = "flex";
+} else {
+    lang.style.display = "none";
+}
+});
+
+document.getElementById("project").addEventListener("click",function(){
+    var pro = document.getElementById("projects");
+if (pro.style.display === "none") {
+    pro.style.display = "flex";
+} else {
+    pro.style.display = "none";
+}
+});
